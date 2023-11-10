2358844e7a682bd568ad52d71c94d14ae19f4020
Fernanda Leite <edinaldofpinheiro@gmail.com>
2023-11-10 09:58:20 -0300

    segundo commit, acabi :)

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
-app.get('/' , (requisicao, resposta) =>  {
-    resposta.send("Estou funcionando com express")
+const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/' , (requisicao, resposta) =>  {
+    resposta.sendFile(`${caminhoBase}/index.html`)
+    
 })
 
 app.listen(3000 , () => {
